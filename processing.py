32fdac30d9b4a3bc9eca437e4db31dae8b41627a
Stefan Dalecki <stefan.dalecki@colorado.edu>
2022-02-25 11:45:50 -0700

    cleaner display handling

--- processing.py
+++ processing.py
@@ -25,15 +25,22 @@ def main():
     """
 
     file_format = input('Analyze csv or nd2 files? (csv or nd2)\n')
-    script = io.Setup(file_format = file_format)
+    # file_format = 'csv'
+    script = io.Setup(file_format)
     folder = script.rootdir
     outfile = script.savefile
     bright_method = input('Brightness thresholding method? (manual or auto)\n')
-    display = False
+    # bright_method = 'auto'
+    display_ans = input('Display figures? (y/n)\n')
+    # display_ans = 'n'
+    if display_ans == 'y':
+        display = True
+    else:
+        display = False
     count = 1
     try:
         print('\nBeginning Analyses\n')
-        for subdir, dirs, files in os.walk(folder):
+        for subdir, dirs, files in os.walk(folder, topdown=False):
             for file in files:
                 if file.endswith(script.filetype):
                     print(file)
@@ -42,20 +49,21 @@ def main():
                     print(subdir)
                     movie = script.dfread(subdir, file)
                     if bright_method == 'manual':
-                        cut.Brightness.manual(movie, movie.df, display=display)
+                        cut.Brightness.manual(movie, movie.df, display)
                     if bright_method == 'auto':
-                        cut.Brightness.auto(movie, movie.df)
+                        cut.Brightness.auto(movie, movie.df, display)
                     cut.Diffusion.displacement(movie, movie.df)
                     BSL = k.BSL(movie)
                     Ray = k.RayD(movie, movie.onestep_SDs)
                     MSD = k.MSD()
                     cf.FitFunctions.linear(movie, movie.mean_SDs,
                                            'Step Length', 'MSD', MSD)
-                    cf.OneCompExpDecay(movie, BSL, display=display)
-                    cf.TwoCompExpDecay(movie, BSL, display=display)
-                    cf.Alt_TwoCompExpDecay(movie, BSL, display=display)
-                    cf.ExpLinDecay(movie, BSL, display=display)
-                    cf.TwoCompRayleigh(movie, Ray, display=display)
+                    cf.OneCompExpDecay(movie, BSL, display)
+                    cf.TwoCompExpDecay(movie, BSL, display)
+                    cf.Alt_TwoCompExpDecay(movie, BSL, display)
+                    cf.ExpLinDecay(movie, BSL, display)
+                    cf.OneCompRayleigh(movie, Ray, display)
+                    cf.TwoCompRayleigh(movie, Ray, display)
                     if count == 1:
                         todays_movies = io.Exports(
                             outfile, movie.exportdata, folder)
