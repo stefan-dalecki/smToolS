e461ee5b14fdbb4d29a53507de4de11264a13d00
Stefan Dalecki <stefan.dalecki@colorado.edu>
2022-03-04 10:11:58 -0700

    excel writer

--- inout.py
+++ inout.py
@@ -69,6 +69,7 @@ class Setup:
                     subdir = subdir.replace('\\', '/')
                     filepath = f'{subdir}/{file}'
                     self.filelist += [filepath]
+        self.filelist.sort(reverse=False)
 
     def dfread(self, filepath):
         """
@@ -260,6 +261,58 @@ class Exports:
     def start_df(self, exportdata, rootdir):
         start = pd.DataFrame(columns=list(exportdata.keys()))
         self.export_df = start
+
+    def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False,
+                       **to_excel_kwargs):
+        """
+        Add row to Excel
+
+        Use Excel Writer to add row to xlsx file
+
+        Args:
+            filename (str): Excel filename
+            df (df): dataframe to append
+            sheet_name (str): Excel file sheet tab name
+            startrow (int): row to append data
+            truncate_sheet (bool): shorten sheet
+            **to_excel_kwargs (dict): additional arguments
+
+        Returns:
+            new excel file
+
+        Raises:
+            none
+
+        """
+
+        if not os.path.isfile(filename):
+            df.to_excel(
+                filename,
+                sheet_name=sheet_name,
+                startrow=startrow if startrow is not None else 0,
+                **to_excel_kwargs)
+            return
+        if 'engine' in to_excel_kwargs:
+            to_excel_kwargs.pop('engine')
+        options = {}
+        options['strings_to_formulas'] = False
+        options['strings_to_urls'] = False
+        writer = pd.ExcelWriter(filename, engine='openpyxl', mode='a',
+                                options=options)
+        writer.book = load_workbook(filename)
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            idx = writer.book.sheetnames.index(sheet_name)
+            writer.book.remove(writer.book.worksheets[idx])
+            writer.book.create_sheet(sheet_name, idx)
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+        if startrow is None:
+            startrow = 0
+        df.to_excel(writer, sheet_name, startrow=startrow,
+                    **to_excel_kwargs)
+        writer.save()
 
     def build_df(self, dict):
         """
