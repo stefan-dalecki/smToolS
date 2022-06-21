7afb4c777db9e950a699f248ac063b20a04abb80
Stefan Dalecki <stefan.dalecki@colorado.edu>
2022-06-21 14:22:23 -0600

    ok

--- tests.py
+++ tests.py
@@ -1,24 +1,4 @@
-class Bright:
-    def __init__(self, method):
-        self._options = [
-            option for option in dir(Bright) if option.startswith("__") is False
-        ]
-        assert method in self._options, "Chosen method is not available"
-        self(method)
-
-    def __call__(self, method):
-        func = getattr(Bright, method)
-        func(self, "test")
-
-    def automatic(self, name):
-        print(name)
-        print("auto")
-
-    def manual(self):
-        print("man")
-
-    def none(self):
-        print("none")
-
-
-b = Bright("automatic")
+ex = [1, 2, 5]
+am = 11
+print(am // 5)
+print(1 // 2)
