5414d7e77d3000947c68ae4176e9eae96812dff1
Stefan Dalecki <stefan.dalecki@colorado.edu>
2022-07-20 12:01:46 -0600

    decorator documentation

--- decorators.py
+++ decorators.py
@@ -5,9 +5,21 @@ from typing import Callable
 
 
 class Progress:
+    """Decorator functions for tracking script progress"""
+
     @staticmethod
     def details(func: Callable):
+        """Gives details on overall script progress
+
+        Args:
+            func (Callable): main function in smTIRF.py file
+
+        Returns:
+            wrapper (Callable): inner wrapper function
+        """
+
         def wrapper(*args, **kwargs):
+            """Inner wrapper function"""
             begin = time.time()
             print("Beginning Movie Analysis...", end="\n" * 2)
             func(*args, **kwargs)
@@ -23,8 +35,23 @@ class Progress:
 
     @staticmethod
     def start_finish(*, indent: int = 0, action: str, ending: str):
+        """Tracks only start and finish of function
+
+        Args:
+            action (str): action taking place
+            ending (str): ending of descriptive string
+            indent (int, optional): how far to indent print statement. Defaults to 0.
+        """
+
         def inner(func: Callable):
+            """Inner function of decorator
+
+            Args:
+                func (Callable): function acted upon
+            """
+
             def wrapper(*args, **kwargs):
+                """Inner wrapper function"""
                 print(f"{'   '*indent}Starting {action} {ending}")
                 func(*args, **kwargs)
                 print(f"{'   '*indent}Finished {action} {ending}", end="\n" * 2)
