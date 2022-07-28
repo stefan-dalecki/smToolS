acee7d8e63308d05734eb6ab17cc6f086235d614
Stefan Dalecki <sjdalecki@gmail.com>
2022-07-27 21:29:16 -0600

    visualization progress

--- Visualization/3D_View.py
+++ Visualization/3D_View.py
@@ -47,8 +47,9 @@ class Reader:
 
 
 class Analyze:
-    def __init__(self, df: pd.DataFrame):
-        self._df = df
+    def __init__(self, metadata: object, df: pd.DataFrame):
+        self._metadata = metadata
+        self.df = df
 
     def trio_lyze(self):
         """Calculate average brightness, length, and diffusivity for each trajectory
@@ -59,20 +60,20 @@ class Analyze:
         Returns:
             pd.DataFrame: updated trajectory data with trio
         """
-        self._df["Average_Brightness"] = self._df.groupby("Trajectory")[
+        self.df["Average_Brightness"] = self.df.groupby("Trajectory")[
             "Brightness"
         ].transform(np.mean)
-        self._df["Length (frames)"] = self._df.groupby("Trajectory")[
+        self.df["Length (frames)"] = self.df.groupby("Trajectory")[
             "Trajectory"
         ].transform("size")
-        data = self._df.groupby("Trajectory")[["x", "y"]].apply(fo.Calc.one_step_MSD)
+        data = self.df.groupby("Trajectory")[["x", "y"]].apply(fo.Calc.one_step_MSD)
         data = pd.DataFrame(data.to_list(), columns=["SDs", "MSD"])
         data.index += 1
         data.reset_index(inplace=True)
         data = data.rename(columns={"index": "Trajectory"})
-        self._df["SDs"] = reduce(operator.add, data["SDs"])
-        self._df = self._df.merge(data[["Trajectory", "MSD"]])
-        self._df = self._df.assign(Group="")
+        self.df["SDs"] = reduce(operator.add, data["SDs"])
+        self.df = self.df.merge(data[["Trajectory", "MSD"]])
+        self.df = self.df.assign(Group="")
         return self
 
     def identify(
@@ -82,6 +83,60 @@ class Analyze:
         min_length: dict = {"short": 10},
         diffusion: dict = {"slow": 0.3, "fast": 3.5},
     ):
-        assert min_length["short"] < np.max(self._df["Length (frames)"])
-        
-    def identifying_function()
+        assert min_length["short"] < np.max(self.df["Length (frames)"])
+        for row in self.df.iterrows:
+            if row["Average_Brightness"] < brightness["dim"]:
+                row["ID"] += "dim + "
+            if row["Average_Brightness"] > brightness["bright"]:
+                row["ID"] += "bright + "
+            if row["Length (frames)"] < min_length["short"]:
+                row["ID"] += "short + "
+            if row["MSD"] < diffusion["slow"]:
+                row["ID"] += "slow + "
+            if row["MSD"] > diffusion["fast"]:
+                row["ID"] += "fast + "
+        self.df["ID"] = self.df["ID"].removesuffix(" + ").fillna("valid trajectory")
+        return self
+
+
+class Plot:
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        *,
+        x: str = "Average_Brightness",
+        y: str = "Length (frames)",
+        z: str = "MSD",
+    ):
+        self.df = df[["Trajectory", x, y, z, "ID"]]
+        self._x = df[x]
+        self._y = df[y]
+        self._z = df[z]
+        self._x_label = x
+        self._y_label = y
+        self._z_label = y
+        self._title = "Trajectories"
+
+    def figure(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+        ax.set_x_label(self._x_label, labelpad=10)
+        ax.set_y_label(self._y_label, labelpad=10)
+        ax.set_z_label(self._z_label, labelpad=10)
+        ax.set_title(self._title)
+        sc = ax.scatter3D(
+            self._x,
+            self._y,
+            self._z,
+            c=self.df["ID"],
+            cmap="Dark2",
+        )
+        ax.view_init(30, 60)
+        plt.legend(*sc.legend_elements(), title="Group ID")
+        plt.show()
+
+
+meta = Metadata()
+file = Reader()
+data = Analyze(meta, file)
+figure = Plot(data.df)
